5cc113068a00e49d19610bf8c2b905f5985bc78c
AdamHaluska <adam.haluska11@gmail.com>
2021-11-14 21:11:19 +0100

    hotovo!

--- Open-Lab-03.05/Comparator.cs
+++ Open-Lab-03.05/Comparator.cs
@@ -4,9 +4,12 @@ namespace Open_Lab_03._05
 {
     public class Comparator
     {
-        public bool MatchCaseInsensitive(string str1, string str2)
+       if (String.Equals(str1, str2, StringComparison.OrdinalIgnoreCase))
+                return true;
+            
         {
-            throw new NotImplementedException();
+            else
+                return false;
         }
     }
 }
